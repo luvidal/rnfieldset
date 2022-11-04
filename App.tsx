7e3f8579ec7bf66fb75df5edad7a622950e9a2d3
luvidal <luvidal@edictus.com>
2022-11-04 11:38:58 -0300

    1st Commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ReactNode } from 'react'
+import { StyleSheet, View, Text, useColorScheme } from 'react-native'
 
-export default function App() {
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+interface StyleColors { lightfore:string, lightback:string, darkfore:string, darkback:string }
+interface FieldProps  { title:string; children:ReactNode; colors?:StyleColors }
+
+const Fieldset = ( { title, children = undefined, colors = undefined }:FieldProps ) => 
+{
+  const defaults = { darkfore:'#666', darkback:'#111', lightfore:'#BBB', lightback:'#F0F0F0' } // default palette
+  const c = Object.assign({}, defaults, colors) // the palette is full now
+
+  const dark = (useColorScheme() || 'light') == 'dark'
+
+  const color = dark ? c.darkfore : c.lightfore
+  const backgroundColor = dark ? c.darkback : c.lightback
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[ fieldset.box, { borderColor:color, backgroundColor } ]}>
+      <Text style={[ fieldset.label, { color, backgroundColor } ]}>   { title }   </Text> 
+      { children }
     </View>
-  );
-}
+)}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+export default Fieldset
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+const fieldset = StyleSheet.create(
+{
+  box:{
+    borderWidth:1,
+    borderRadius:4,
+    padding:20,
+    margin:20,
+  },
+
+  label:{
+    position:'absolute', 
+    top:-9, 
+    left:11,
   },
 });
